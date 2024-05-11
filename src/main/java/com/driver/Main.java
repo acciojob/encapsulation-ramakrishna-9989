7bde702858885a7e91f67293973d766d15492587
Rama Krishna <raamakrishna7729@gmail.com>
2024-05-11 12:48:06 +0530

    Krishna

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        // Task 3
+        RWOnly obj = new RWOnly();
+
+        // Task 4
+        // obj.name = "John"; // Error: 'name' has private access in RWOnly
+        // System.out.println(obj.name); // Error: 'name' has private access in RWOnly
+
+        // Task 6
+        obj.setName("John");
+        System.out.println(obj.getName()); // Should print "John"
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    // Task 5
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
